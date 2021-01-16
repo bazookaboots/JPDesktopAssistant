e524daf6800b754229dfe0ef40d59d944856b4b9
baygoo <62280926+bay-goo@users.noreply.github.com>
2021-01-16 12:49:36 -0800

    You need to build before you run the GUI and the google json files needs to be in the folder labeled PAL

--- PAL/Main/Program.cs
+++ PAL/Main/Program.cs
@@ -15,7 +15,6 @@ namespace PAL.Core
             //Listener waiting for stuff from speech listener
             SpeechListener PAL = new SpeechListener();
             List<string> results = (List<string>)await PAL.Start(10);
-            Console.WriteLine(results.First()); 
         }
     }
 }
