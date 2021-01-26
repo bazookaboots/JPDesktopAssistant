3f10e3919a00836269e16af7c428e0bb7a37b6d9
baygoo <62280926+bay-goo@users.noreply.github.com>
2021-01-26 11:23:13 -0800

    There you go Eren

--- PAL/Main/Program.cs
+++ PAL/Main/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Remote;
 
 namespace PAL.Core
 {
@@ -13,9 +16,20 @@ namespace PAL.Core
             //Listener waiting for stuff from GUI 
 
             //Listener waiting for stuff from speech listener
-            SpeechListener PAL = new SpeechListener();
-            List<string> results = (List<string>)await PAL.Start(10);
-            Console.WriteLine(results.First()); 
+            //SpeechListener PAL = new SpeechListener();
+            //List<string> results = (List<string>)await PAL.Start(10);
+            //Console.WriteLine(results.First()); 
+
+            IWebDriver driver = new ChromeDriver(@"C:\WebDriver\bin");
+            driver.Navigate().GoToUrl(@"https://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("Mommy Milkers");
+            driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+            driver.Navigate().GoToUrl(@"https://www.youtube.com");
+            driver.FindElement(By.Name("search_query")).SendKeys("Ignition Remix");
+            driver.FindElement(By.Name("search_query")).SendKeys(Keys.Enter);
+            driver.FindElement(By.Name("Ignition (Remix)")).Click();
+
+            //driver.Quit();
         }
     }
 }
