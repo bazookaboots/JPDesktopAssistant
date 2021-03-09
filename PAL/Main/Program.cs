1c9a1c04e88e670890c9f39f271fff84599a6147
Eren Bikmaz <eren.bikmaz@oit.edu>
2021-03-09 14:49:42 -0800

    New control loop. option flags. pal can talk.

--- PAL/Main/Program.cs
+++ PAL/Main/Program.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using System.Threading;
 using OpenQA.Selenium.Remote;
 using System.Collections;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
 
 namespace PAL.Core
 {
@@ -25,30 +27,69 @@ namespace PAL.Core
         }
         static async Task Main(string[] args)
         {
-            //Listener waiting for stuff from GUI 
-            // Beep beep boop boop remember to stop and take a poop!
-            //Listener waiting for stuff from speech listener
-            SpeechListener PAL = new SpeechListener();
-            string results = ((List<string>)await PAL.Start(8)).First();
-            Console.WriteLine(results.First());
+            //boolean settings defined here, 
+            //these can be moved elsewhere to connect/read from gui as long as they can be accessed in this scope
+            bool pal = true; //controls whether this process should continue running in background
+            bool vocal = true; //determines whether pal should speak back or not
+            bool listenting = true; // controls always-on, always-listening functionality (hey pal)
+            bool active = false; // when true, pal will respond to commands (set in hey Pal scope or hotkey scope)
 
-            //send results to parser for processing
-            Parser parser = new Parser();
-            Queue transcript = parser.Tokenize(results);
+            var synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
 
-            //pass tokenized string to parser, which returns a command to execute
-            Command cmd = parser.Parse(transcript);
-            cmd.Print();
+            if (vocal) synthesizer.Speak("Pal is alive.");
+            if (vocal && listenting) synthesizer.Speak("Just call if you need me.");
 
-            try
+            while (pal)
             {
-                new Executer().ExecuteCommand(cmd).Wait();
-            }
-            catch (AggregateException e)
-            {
-                foreach (var ex in e.InnerExceptions)
+
+                if (active)
                 {
-                    Console.WriteLine("ERROR: " + ex.Message);
+                    //Initialize Google Listener
+                    SpeechListener PAL = new SpeechListener();
+                    string results;
+                    try
+                    {
+                        results = ((List<string>)await PAL.Start(8)).First();
+                    }
+                    catch 
+                    {
+                        results = " ";
+                    }
+                    Console.WriteLine(results.First());
+
+                    //send results to parser for processing
+                    Parser parser = new Parser();
+                    Queue transcript = parser.Tokenize(results);
+
+                    //pass tokenized string to parser, which returns a command to execute
+                    Command cmd = parser.Parse(transcript);
+                    cmd.Print();
+
+                    //attempt to execute the generated command
+                    try
+                    {
+                        new Executer().ExecuteCommand(cmd).Wait();
+                    }
+                    catch (AggregateException e)
+                    {
+                        foreach (var ex in e.InnerExceptions)
+                        {
+                            Console.WriteLine("ERROR: " + ex.Message);
+                        }
+                    }
+                }
+                else if (listenting) // heyPal Functionality
+                {
+                    //if (recognizer.Result.Text.ToLowerInvariant().ToString() == "hey pal")
+                    {
+                        if (vocal) synthesizer.Speak("Yes master?");
+                        active = true;
+                    }
+                }
+                else 
+                { 
+                //keypress could also set active flag
                 }
             }
             
