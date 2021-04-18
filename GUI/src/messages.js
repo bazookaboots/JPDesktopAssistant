155e7a6e82da7dd34ccd0fbb2e0bf4f3f6d29257
Morgan Anderson <themorgander@gmail.com>
2021-04-17 21:12:39 -0700

    Added paths, cleanup, updated documentation

--- GUI/src/messages.js
+++ GUI/src/messages.js
@@ -1,8 +1,6 @@
 const io = require("socket.io-client"),
 ioClient = NULL
 
-
-// This function is automatic
 ioClient.on("getmessage", (msg) => {
     console.info(msg)
     //TODO: Add logic for the user to see these messages in the GUI
@@ -10,46 +8,153 @@ ioClient.on("getmessage", (msg) => {
 })
 
 //TODO: This function needs to run when pal starts and it needs the user id
-function Startup(m_userid)
+function Startup(id)
 {
     ioClient = io.connect("http://localhost:8000", 
     { 
         query: {
-        userid: m_userid
+        userid: id
     }})
     
-    GetAllMessages(m_userid)
+    GetAllMessages(id)
 }
 
 //TODO: This function needs to run when the user sends a message in the GUI
 //You will need: message, toid, fromid
-function SendMessage(m_message, m_toid, m_fromid) {
+function SendMessage(mess, to, from) {
     const request = {
-        message: m_message,
-        toid: m_toid,
-        fromid: m_fromid
+        message: mess,
+        toid: to,
+        fromid: from
     }
 
     ioClient.emit("sendmessage", request)
 }
 
-//TODO: This function needs to run hen the user deletes a message
-//You will need: messageid
-//This should call the backend path "/deletemessage"
-function DeleteMessage(m_messageid){
+/**
+ * Sends an HTTP POST request to the backend asking if it can
+ * delete a message
+ * @name DeleteMessage
+ * @memberof messages.js
+ * @param {int} messid - int representing the desired message
+ */
+ async function DeleteMessage(messid) {
 
+    const data = JSON.stringify({
+        messageid: messid
+    })
+
+    console.log("Called DeleteMessage");    //DEBUG
+
+    let options = {
+        host: hostURL,
+        path: '/deletemessage',
+        port: 3010,
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    };
+
+    const request = http.request(options, response => {
+        console.log(`statusCode: ${response.statusCode}`)
+        response.on('data', d => {
+            console.log(d);
+        })
+    })
+
+    request.on('error', error => {
+        console.error(error)
+    })
+ 
+    request.write(data)
+
+    request.end()
 }
 
-//TODO: This function needs to run hen the user displays a single users message history
-//You will need: fromid, toid
-//This should call the backend path "/getusermessages"
-function GetUserMessages(m_fromid, m_toid){
+/**
+ * Sends an HTTP POST request to the backend asking if it can
+ * delete a message
+ * @name GetUserMessages
+ * @memberof messages.js
+ * @param {int} to - int representing the user message was sent to
+ * @param {int} from -  int representing the user message was sent from
+ */
+ async function GetUserMessages(to, from) {
 
+    const data = JSON.stringify({
+        toid: to,
+        fromid: from
+    })
+
+    console.log("Called GetUserMessages");    //DEBUG
+
+    let options = {
+        host: hostURL,
+        path: '/getusermessages',
+        port: 3010,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    };
+
+    const request = http.request(options, response => {
+        console.log(`statusCode: ${response.statusCode}`)
+        response.on('data', d => {
+            console.log(d);
+        })
+    })
+
+    request.on('error', error => {
+        console.error(error)
+    })
+
+    request.write(data)
+
+    request.end()
 }
 
-//TODO: This function needs to run hen the user logs in
-//You will need: userid (fromid)
-//This should call the backend path "/getallmessages"
-function GetAllMessages(m_fromid){
-    
+/**
+ * Sends an HTTP POST request to the backend asking if it can
+ * delete a message
+ * @name GetAllMessages
+ * @memberof messages.js
+ * @param {int} userid - int representing the user
+ */
+ async function GetAllMessages(userid) {
+
+    const data = JSON.stringify({
+        fromid: userid
+    })
+
+    console.log("Called GetAllMessages");    //DEBUG
+
+    let options = {
+        host: hostURL,
+        path: '/getallmessages',
+        port: 3010,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    };
+
+    const request = http.request(options, response => {
+        console.log(`statusCode: ${response.statusCode}`)
+        response.on('data', d => {
+            console.log(d);
+        })
+    })
+
+    request.on('error', error => {
+        console.error(error)
+    })
+
+    request.write(data)
+
+    request.end()
 }
