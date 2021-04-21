e02efca941f8af3a6ddd04d51782c238ca0e8d9d
baygoo <62280926+bay-goo@users.noreply.github.com>
2021-04-21 12:26:21 -0700

    Create index.js

---
+++ Overlay/index.js
@@ -0,0 +1,54 @@
+/*
+ * Player.GG Simple Game Overlay
+ */
+
+const path = require("path");
+const {app, BrowserWindow, ipcMain, shell} = require("electron");
+
+function onReady() {
+
+    const win = new BrowserWindow({
+        title: "PAL Overlay",
+        x: 0,
+        y: 0,
+        width: 600,
+        height: 400,
+        transparent: true,
+        opacity: 0.9,
+        frame: false,
+        alwaysOnTop: true,
+        movable: true,
+        resizable: true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // Open the developer tools
+//     win.webContents.openDevTools();
+
+    // and load the index.html of the app.
+//     win.loadFile("docs/index.html");
+    win.loadURL("https://www.youtube.com/");
+    win.setIgnoreMouseEvents(false);
+    win.setMovable(true);
+
+//     win.on("page-title-updated", (evt) => {
+//         evt.preventDefault();
+//     });
+
+    ipcMain.on("pgg-minimize-app", () => {
+        win.minimize();
+    });
+
+    ipcMain.on("pgg-close-app", () => {
+        win.close();
+    });
+
+    ipcMain.on("pgg-open-official-site", () => {
+        shell.openExternal("https://www.player.gg/tools/simple-game-overlay/");
+    });
+
+}
+
+app.on("ready", onReady);
